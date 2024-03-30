bc6495a7097c251dc2cb2655480b5890fd9922cf
Bogdan Boyko <boyko.bogdan45@gmail.com>
2024-03-30 21:51:07 +0200

    feature: added more tasks functionality

--- server/src/controllers/tasks.controller.ts
+++ server/src/controllers/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Delete,
+  Post,
+  Body,
+} from '@nestjs/common';
 import { TasksService } from '../services/tasks.service';
 import { Task } from '../models/task.model';
 
@@ -14,5 +22,31 @@ export class TasksController {
   @Get('/:list_id')
   getTasksByListId(@Param('list_id') list_id: string): Promise<Task[]> {
     return this.tasksService.getByListId(+list_id);
+  }
+
+  @Post('/create')
+  createTask(@Body() taskData: Partial<Task>): Promise<Task> {
+    return this.tasksService.create(taskData);
+  }
+
+  @Patch('/update/:id')
+  updateTask(
+    @Param('id') id: string,
+    @Body() taskData: Partial<Task>,
+  ): Promise<Task> {
+    return this.tasksService.update(parseInt(id), taskData);
+  }
+
+  @Delete('/delete/:id')
+  deleteTask(@Param('id') id: string): Promise<void> {
+    return this.tasksService.delete(parseInt(id));
+  }
+
+  @Patch('/:taskId/move/:newListId')
+  async moveTask(
+    @Param('taskId') taskId: string,
+    @Param('newListId') newListId: string,
+  ): Promise<Task> {
+    return this.tasksService.move(parseInt(taskId), parseInt(newListId));
   }
 }
--- server/src/services/tasks.service.ts
+++ server/src/services/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Task } from '../models/task.model';
@@ -16,5 +16,38 @@ export class TasksService {
 
   async getByListId(list_id: number): Promise<Task[]> {
     return this.taskRepository.find({ where: { list_id } });
+  }
+
+  async create(taskData: Partial<Task>): Promise<Task> {
+    const newTask = this.taskRepository.create(taskData);
+    return this.taskRepository.save(newTask);
+  }
+
+  async update(taskId: number, taskData: Partial<Task>): Promise<Task> {
+    const task = await this.taskRepository.preload({
+      id: taskId,
+      ...taskData,
+    });
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${taskId} not found.`);
+    }
+    return this.taskRepository.save(task);
+  }
+
+  async delete(taskId: number): Promise<void> {
+    const result = await this.taskRepository.delete(taskId);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with ID ${taskId} not found.`);
+    }
+  }
+
+  async move(taskId: number, newListId: number): Promise<Task> {
+    const task = await this.taskRepository.findOneBy({ id: taskId });
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${taskId} not found.`);
+    }
+
+    task.list_id = newListId;
+    return this.taskRepository.save(task);
   }
 }
